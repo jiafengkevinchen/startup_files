a7578faf33b52d5875967fd30a86b4604030d3c0
Jiafeng Chen <jiafeng.k.chen@gmail.com>
2019-09-02 22:01:08 -0400

    Refactoring code

--- 00-imports.py
+++ 00-imports.py
@@ -1,30 +1,52 @@
-from datetime import datetime
-from time import sleep
+import datetime
+import os
+import sys
+import time
 
-import numpy as np
-from numpy import log, exp, nan
-import pandas as pd
-from pandas import Series, DataFrame
-import seaborn as sns
-from seaborn import regplot
-import matplotlib.pyplot as plt
 import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
-from statsmodels.tools import add_constant
-from tqdm import tqdm_notebook as tqdm
 from IPython import get_ipython
+from numpy import exp, log, nan
+from pandas import DataFrame, Series
+from seaborn import regplot
+from statsmodels.iolib.summary2 import summary_col
+from statsmodels.tools import add_constant
+from tqdm.auto import tqdm
+
+import janitor
+
+tqdm.pandas()
+
+# pip install git+https://github.com/jiafengkevinchen/pandas_tools
+try:
+    import pandas_tools.latex as tex
+except ImportError:
+    print("Run pip install git+https://github.com/jiafengkevinchen/pandas_tools")
 
 
 try:
-    get_ipython().run_line_magic('matplotlib', 'inline')
-    get_ipython().run_line_magic('load_ext', 'rpy2.ipython')
+    from janitor.utils import skiperror, skipna
+except ImportError:
+    try:
+        from pandas_tools.latex import skiperror, skipna
+    except ImportError:
+        print("Run pip install git+https://github.com/jiafengkevinchen/pandas_tools")
+
+
+try:
+    get_ipython().run_line_magic("matplotlib", "inline")
+    get_ipython().run_line_magic("load_ext", "rpy2.ipython")
 except:
     pass
 
-sns.set_style('white')
+sns.set_style("white")
 np.set_printoptions(suppress=True)
-pd.set_option('display.float_format', lambda x: '%.4f' % x)
-mpl.rcParams['savefig.dpi'] = 300
-mpl.rcParams['figure.dpi']= 150
+pd.set_option("display.float_format", lambda x: "%.4f" % x)
 
+mpl.rcParams["savefig.dpi"] = 300
+mpl.rcParams["figure.dpi"] = 150
