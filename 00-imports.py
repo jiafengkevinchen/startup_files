4c244d54242e9b4d5237cd7d6e3329630bea39e2
Jiafeng Chen <jiafeng.k.chen@gmail.com>
2021-01-02 13:58:04 -0500

    Adding packages; gitignore

--- 00-imports.py
+++ 00-imports.py
@@ -1,12 +1,16 @@
 import datetime
+import json
 import os
+import pickle
 import sys
 import time
 
+import janitor
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import scipy
 import seaborn as sns
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
@@ -17,8 +21,6 @@ from seaborn import regplot
 from statsmodels.iolib.summary2 import summary_col
 from statsmodels.tools import add_constant
 from tqdm.auto import tqdm
-
-import janitor
 
 tqdm.pandas()
 
@@ -50,3 +52,26 @@ pd.set_option("display.float_format", lambda x: "%.4f" % x)
 
 mpl.rcParams["savefig.dpi"] = 300
 mpl.rcParams["figure.dpi"] = 150
+
+
+def save_pickle(obj, path):
+    with open(path, "wb") as f:
+        pickle.dump(obj, f)
+
+
+def save_json(obj, path):
+    with open(path, "w") as f:
+        json.dump(obj, f)
+
+
+def load_json(path):
+    with open(path, "r") as f:
+        obj = json.load(f)
+    return obj
+
+
+def load_pickle(path):
+    with open(path, "rb") as f:
+        obj = pickle.load(f)
+    return obj
+
